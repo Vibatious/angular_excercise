b1fbfe4a7c4897f218f1f6f4c3b18cbebab2234f
Vibatious <varshneylove1998@gmail.com>
2020-05-08 15:26:42 +0530

    Calculator

--- calcExcercise/src/app/app.component.ts
+++ calcExcercise/src/app/app.component.ts
@@ -7,4 +7,42 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'calcExcercise';
+  display:string ="none";
+  op_sign:string ="";
+  num1 :number = 0;
+  num2 :number = 0;
+  answer:number = 0;
+  operation = (op) => {
+    switch(op){
+      case 'add': this.answer = Number(this.num1)+ Number(this.num2);
+                  this.op_sign = "+";
+                  if(this.display == 'none'){
+                  this.display = 'inline-block';
+                  }
+              break;
+      case 'sub': this.answer = Number(this.num1) - Number(this.num2);
+                  this.op_sign = "-";
+                  if(this.display == 'none'){
+                  this.display = 'inline-block';
+                  }
+              break;
+      case 'mul': this.answer = Number(this.num1) * Number(this.num2);
+                  this.op_sign = "*";
+                  if(this.display == 'none'){
+                  this.display = 'inline-block';
+                  }
+              break;
+      case 'div': this.answer = Number(this.num1) / Number(this.num2);
+                  this.op_sign = "/";
+                  if(this.display == 'none'){
+                  this.display = 'inline-block';
+                  }
+              break;
+      case 'clear':this.op_sign = "op";
+                  this.answer=this.num1=this.num2=0;
+                  this.display = "none";
+              break;
+
+    }
+  }
 }
